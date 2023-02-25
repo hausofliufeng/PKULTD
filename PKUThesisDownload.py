99e4ca33a7d842795197aefadd6fef223df57e71
liufeng <liufeng2900@gmail.com>
2023-02-25 16:45:31 +0800

    update

--- PKUThesisDownload.py
+++ PKUThesisDownload.py
@@ -7,19 +7,27 @@ from urllib.request import urlretrieve
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
-driver = webdriver.Chrome()
+
+# option = webdriver.ChromeOptions()
+# option.add_argument('--user-data-dir=/Users/liufeng/Library/Application Support/Google/Chrome') # 设置成自己的数据目录
+# option.add_argument(" --profile-directory=Default")
+
+# driver = webdriver.Chrome("./chromedriver",options=option)
+
+driver = webdriver.Chrome("./chromedriver")
+time.sleep(30)
 
 #论文链接
-thsis_url="https://thesis.lib.pku.edu.cn/docinfo.action?id1=6253242fa7b50a1e05bceb7008db2855&id2=4JrF8NUQsEQ%253D"
+thsis_url="https://drm.lib.pku.edu.cn/pdfindex.jsp?fid=d349749648fb6cffc255fd00f09eac56"
 driver.get(thsis_url)
 driver.refresh()
 time.sleep(2)
-lookbut = driver.find_element_by_link_text('查看全文')
-lookbut.click()
-handles = driver.window_handles
-driver.switch_to.window(handles[1])
-time.sleep(2)
-#获取总页数
+# lookbut = driver.find_element_by_link_text('查看全文')
+# lookbut.click()
+# handles = driver.window_handles
+# driver.switch_to.window(handles[1])
+# time.sleep(2)
+# #获取总页数
 tpage=driver.find_element_by_css_selector('span#totalPages.toolbar-page-num')
 total_pages=int(re.sub("\D","",tpage.get_attribute("innerText")))
 print('本论文总页数为：%d 页'%(total_pages))
